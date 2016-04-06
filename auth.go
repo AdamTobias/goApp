3c77fc147bf24e73c6f032fd48e45a1d4c0e14dc
Adam Grimm <ATGrimm@gmail.com>
2016-04-05 19:21:54 -0700

    Refactored auth service to remove DB access and now interface with the DBLayer

--- auth.go
+++ auth.go
@@ -3,37 +3,24 @@ package main
 import (
   "net/http"
   "fmt"
-  "database/sql"
-  _ "github.com/go-sql-driver/mysql"
   "golang.org/x/crypto/bcrypt"
   "github.com/dgrijalva/jwt-go"
   "time"
+  "io/ioutil"
+  "encoding/json"
+  "bytes"
 )
 
-type database struct {
-  con *sql.DB
+type User struct {
+  Id string
+  Username string
+  Password string
 }
-
-// initializes the database connection
-func (d *database) dbInit() {
-  // db, err := sql.Open("mysql", "root:rodam@tcp(127.0.0.1:5587)/gopractice")
-  dbConnection, err := sql.Open("mysql", "root:rodam@/gopractice")
-  if err != nil {
-    fmt.Println("error connecting to db")
-    return
-  }
-  d.con = dbConnection
-}
-
-//create a global db struct for anyone to use
-var db database
 
 func main () {
   http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
   http.HandleFunc("/", reqHandler)
   fmt.Println("Listening on 8080")
-  db.dbInit()
-  defer db.con.Close()
   http.ListenAndServe(":8080", nil)
 }
 
@@ -49,70 +36,64 @@ func reqHandler (w http.ResponseWriter, r *http.Request) {
 }
 
 func errorHandler(task string, err error) {
-  fmt.Printf("Got an error trying to %s: %v\n", task, err)
+  if err != nil {
+    fmt.Printf("Got an error trying to %s: %v\n", task, err)
+  }
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-  var id int64
-  var hashedPass []byte
   username := r.FormValue("usernameLI")
   password := r.FormValue("passwordLI")
-  rows, err := db.con.Query("select id, password from users where username = ?", 
-    username)
 
-  if err != nil {
-    errorHandler("querying DB", err)
-  }
+  url := "http://localhost:8000/users?username=" + username
+  resp, err := http.Get(url)
+  errorHandler("making GET request to " + url, err)
+  
+  defer resp.Body.Close()   
+  body, err2 := ioutil.ReadAll(resp.Body)
+  errorHandler("reading request body", err2)
+  
+  var retrievedUser User
+  err = json.Unmarshal(body, &retrievedUser)
+  errorHandler("Unmarshaling response body", err)
 
-  defer rows.Close()
-  for rows.Next() {
-    err := rows.Scan(&id, &hashedPass)
-    if err != nil {
-      errorHandler("scanning rows", err)
-    }
-  }
-  err = rows.Err()
-  if err != nil {
-    errorHandler("scanned rows are messed up", err)
-  }
-  if bcrypt.CompareHashAndPassword(hashedPass, []byte(password)) == nil {
-    jwt := createJWT(id)
-    sendJWT(w, jwt)
-  } else {
-    fmt.Println("user does not exist")
-  }
+  if retrievedUser.Username != "" {
+    if bcrypt.CompareHashAndPassword([]byte(retrievedUser.Password), []byte(password)) == nil {
+      jwt := createJWT(retrievedUser.Id)
+      sendJWT(w, jwt)
+      return
+    } 
+  } 
+  w.Write([]byte("wrong bloke, mate"))
+
 }
 
 func signup (w http.ResponseWriter, r *http.Request) {
   username := r.FormValue("usernameSU")
   password := r.FormValue("passwordSU")
-  rows, err := db.con.Query("select id from users where username = ?", username)
-  
-  if err != nil {
-    errorHandler("querying DB", err)
-  }
-
-  defer rows.Close()
-  for rows.Next() {
-    return
-  }
   
   hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-  if err != nil {
-    errorHandler("generating hash", err)
-  }
+  errorHandler("generating hash", err)
 
-  stmt, err := db.con.Prepare("INSERT INTO users(username, password) VALUES (?, ?)")
-  if err != nil {
-    errorHandler("preparing to write to DB", err)
+  body, err := json.Marshal(map[string]string{"Username":username, "Password":string(hashedPass)})
+  errorHandler("Marshaling body", err)
+
+  url := "http://localhost:8000/users"
+  resp, err := http.Post(url, "application/json", bytes.NewReader(body))
+  errorHandler("making POST request to " + url, err)
+  //make HTTP request to the DBLayer to attempt to add the user
+
+  defer resp.Body.Close()   
+  insertId, err2 := ioutil.ReadAll(resp.Body)
+  errorHandler("reading request body", err2)
+  
+  id := string(insertId)
+  if id != "" {
+    jwt := createJWT(id)
+    sendJWT(w, jwt) 
+  } else {
+    w.Write([]byte("username taken mate"))
   }
-  user, err := stmt.Exec(username, hashedPass)
-  if err != nil {
-    errorHandler("writing to DB", err)
-  }
-  insertId, _ := user.LastInsertId()
-  jwt := createJWT(insertId)
-  sendJWT(w, jwt)
 }
 
 func sendJWT (w http.ResponseWriter, jwt string) {
@@ -123,21 +104,20 @@ func sendJWT (w http.ResponseWriter, jwt string) {
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
   path := r.URL.Path
-  if path == "/login" {
+  if path == "/login" { //login should be a get
     login(w, r)
   } else if path == "/signup" {
     signup(w, r)
   }
 }
 
-func createJWT(userId int64) string {
+func createJWT(userId string) string {
   token := jwt.New(jwt.SigningMethodHS256)
   token.Claims["userId"] = userId
   token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
   tokenString, err := token.SignedString([]byte("REPLACE_WITH_A_SECRET"))
-  if err != nil {
-    errorHandler("signing token", err)
-  }
+  errorHandler("signing token", err)
+  
   return tokenString
 }
 
